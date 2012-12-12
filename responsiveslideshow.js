7c60a5fc7e47dafeb65fda5627a28d84748d1fed
fuzzlywalrus <greg@heroweb.com>
2012-12-12 15:11:31 -0800

    Init Commit
    The very first Commit

---
+++ responsiveslideshow.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('#slideshow').after('<ul id="pager">').cycle({ 
+		fx:     'fade',
+		//prev: '#prev',
+		//next: '#next',
+		pager:  '#pager',
+		containerResize: 0,
+		width: '100%',
+		fit: 1,
+  	pagerAnchorBuilder: function(idx, el) {
+    return '<li><a href="cpage_7#"></a></li>';
+    }
+	});  
+
+});
