dbb31a7ff0dd87cfe0d740f1dd9465ea8f076f4b
fuzzlywalrus <greg@heroweb.com>
2012-12-13 15:52:14 -0800

    New code that allows for dynamic resize via JQuery

--- responsiveslideshow.js
+++ responsiveslideshow.js
@@ -8,8 +8,24 @@ $(document).ready(function() {
 		containerResize: 0,
 		width: '100%',
 		fit: 1,
-  	pagerAnchorBuilder: function(idx, el) {
-    return '<li><a href="cpage_7#"></a></li>';
-    }
+	  	pagerAnchorBuilder: function(idx, el) {
+	    return '<li><a href="cpage_7#"></a></li>';
+	    }
 	});
+	
+//This resizes the blank image in the resize example
+	//initglitch fix 
+	var initfix = 18;
+	
+	///on resize, set blank image height
+	function imageresize() { 
+		var imgHeight = $("#slide1").height();
+		$("#blankimg").css("height", imgHeight+initfix);
+	}
+	$(window).bind("resize", function(){
+		//clears the initfix
+		initfix = 0;
+		imageresize();
+	});  
+	imageresize();
 });
